6da18ec2120aab2a00f5229e093acf879a779ce4
Your Name <youremail@domain.com>
2024-07-29 21:30:42 +0600

    sultan Isaev

--- src/main/java/com/beksons/doa/RetInfoDao.java
+++ src/main/java/com/beksons/doa/RetInfoDao.java
@@ -5,7 +5,7 @@ import com.beksons.entities.RentInfo;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface RetInfoDaoImpl {
+public interface RetInfoDao {
     List<RentInfo> getAllRentInfoBetweenDates(LocalDate checkOut1, LocalDate checkOut2);
 
      Long housesByAgencyIdAndDate(Long agencyId);
